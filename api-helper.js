5b74394182cf894c112449670d7147875ffed6a7
ErdemGhifuri <kesukaan.ultra@gmail.com>
2022-07-18 07:20:03 +0700

    menambahkan halaman bangun datar

---
+++ api-helper.js
@@ -0,0 +1,30 @@
+async function hitApiWithGetMethod(url = '') {
+  // 'tembak' api-nya
+  const response = await fetch(url, {
+    method: 'GET',
+  })
+
+  // Kembalikan respon dari API-nya sebagai JSON / TEXT
+  const clone = response.clone()
+  try {
+    return await response.json()
+  } catch (error) {
+    return await clone.text()
+  }
+}
+
+async function hitApiWithPostMethod(url = '', data = {}) {
+  // 'tembak' api-nya
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data)
+  })
+
+  // Kembalikan respon dari API-nya sebagai JSON / TEXT
+  const clone = response.clone()
+  try {
+    return await response.json()
+  } catch (error) {
+    return await clone.text()
+  }
+}
